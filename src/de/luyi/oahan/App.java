1571402d5eeef9949190142ecfefa517c1651566
lyxhandsome <lyxugly@126.com>
2017-03-13 20:08:25 +0800

    update

--- src/de/luyi/oahan/App.java
+++ src/de/luyi/oahan/App.java
@@ -6,7 +6,6 @@ import java.sql.DriverManager;
 public class App {
 	public static void main(String[] args) {
 		String connectionUrl = "jdbc:sqlserver://localhost:1433;databaseName=master;user=sa;password=myoa888";
-
 		try {
 			// Load SQL Server JDBC driver and establish connection.
 			System.out.print("Connecting to SQL Server ... ");
