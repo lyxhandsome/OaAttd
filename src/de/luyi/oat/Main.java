065f11f0ad61508de2f192f771a468a5b5af5e17
lyxhandsome <lyxugly@126.com>
2017-03-18 20:04:47 +0800

    update

--- src/de/luyi/oat/Main.java
+++
@@ -1,9 +0,0 @@
-package de.luyi.oat;
-
-public class Main {
-//	public static void main(String[] args) throws Exception {
-//		MySQLAccess dao = new MySQLAccess();
-//		dao.readDataBase();
-//		
-//	}
-}
