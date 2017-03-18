17f92dfb3c2ad31ee62f781f1bc72b81ea6044d9
lyxhandsome <lyxugly@126.com>
2017-03-18 21:01:32 +0800

    updte with javase-1.7

--- src/de/luyi/oahan/OaHanvon.java
+++ src/de/luyi/oahan/OaHanvon.java
@@ -1,9 +1,9 @@
 package de.luyi.oahan;
 
-
 public class OaHanvon {
 	public static void main(String[] args) throws Exception {
 		SQLAccess dao = new SQLAccess();
 		dao.readDataBase();
+		System.out.println("Finished!");
 	}
 }
